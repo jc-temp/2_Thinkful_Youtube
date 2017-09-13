0074183324d0e73be8f7e8a0b298d62ade47780d
splnlss <sans.spleen@gmail.com>
2017-09-13 11:13:05 -0400

    added ajax api call to form submit event listener

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search'
+
 $("#youtubesearch").on('submit', (event)=>{
   event.preventDefault()
+  const settings = {
+    url: YOUTUBE_SEARCH_URL,
+    type:'GET',
+    dataType: 'json',
+    success: (data) => {
+      console.log(data)
+    },
+    data:{
+      part: 'snippet',
+      key: 'AIzaSyB2IUbRCb6hYut8z68W1tXzqOUS7GZW6xs',
+      q: $('#q').val()
 
+    }
+
+
+  }
+
+  $.ajax(settings)
 
 }
 
